48895898d06bc1ea2341c375642a0411e36cc0cd
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-10 19:13:33 +0200

    initial variant form type.

---
+++ Form/Type/VariantType.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AssortmentBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+/**
+ * Product variant form type.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class ProductType extends AbstractType
+{
+    /**
+     * Data class.
+     *
+     * @var string
+     */
+    protected $dataClass;
+
+    /**
+     * Constructor.
+     *
+     * It's important to set the data class that was configured inside 'config.yml'.
+     * This will be done automatically when using this class, but if you would like to extend it,
+     * remember to pass '%sylius_assortment.model.variant.class%' as an argument inside service definition.
+     *
+     * @param string $dataClass FQCN of the product variant model
+     */
+    public function __construct($dataClass)
+    {
+        $this->dataClass = $dataClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => $this->dataClass,
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_assortment_variant';
+    }
+}
