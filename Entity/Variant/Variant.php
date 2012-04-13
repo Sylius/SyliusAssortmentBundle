7a30f422ac9bcbefe1102ef1fcbab418d7a5088c
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-13 22:37:16 +0200

    add Variant entity and mapping.

---
+++ Entity/Variant/Variant.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AssortmentBundle\Entity\Variant;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Sylius\Bundle\AssortmentBundle\Model\Variant\Variant as BaseVariant;
+
+/**
+ * Variant entity.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class Variant extends BaseVariant
+{
+}
