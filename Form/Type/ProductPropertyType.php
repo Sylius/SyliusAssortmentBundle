6b7d0c25e8cb13d41c068176c70acb5eef2d97dd
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-13 22:01:14 +0200

    add ProductPropertyType.

---
+++ Form/Type/ProductPropertyType.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AssortmentBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+/**
+ * Product property form type.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class ProductPropertyType extends AbstractType
+{
+    /**
+     * Data class.
+     *
+     * @var string
+     */
+    protected $dataClass;
+
+    /**
+     * Constructor.
+     *
+     * @param string $dataClass FQCN of the product property model
+     */
+    public function __construct($dataClass)
+    {
+        $this->dataClass = $dataClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('properties', 'sylius_assortment_property_choice')
+            ->add('value', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOptions()
+    {
+        return array(
+            'data_class' => $this->dataClass,
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_assortment_product_property';
+    }
+}
