60a1554e2cbd84bdc1edd544efd514d162f980d8
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-13 18:10:23 +0200

    add base CustomizableProduct entity.

---
+++ Entity/CustomizableProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AssortmentBundle\Entity;
+
+use Sylius\Bundle\AssortmentBundle\Model\CustomizableProduct as BaseCustomizableProduct;
+
+/**
+ * Base customizable product entity.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class CustomizableProduct extends BaseCustomizableProduct
+{
+}
