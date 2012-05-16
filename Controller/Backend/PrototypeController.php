34a50d1af53f39d5e12e50fa7b142f1b6c9a6245
umpirsky <umpirsky@gmail.com>
2012-05-16 08:35:36 +0200

    Added prototype option to form.

--- Controller/Backend/PrototypeController.php
+++ Controller/Backend/PrototypeController.php
@@ -38,7 +38,7 @@ class PrototypeController extends Controller
     {
         $prototype = $this->findPrototypeOr404($id);
         $product = $this->container->get('sylius_assortment.manager.product')->createProduct();
-        $form = $this->container->get('form.factory')->create('sylius_assortment_product', $product);
+        $form = $this->container->get('form.factory')->create('sylius_assortment_product', $product, array('prototype' => $prototype));
 
         return $this->container->get('templating')->renderResponse('SyliusAssortmentBundle:Backend/Product:create.html.'.$this->getEngine(), array(
             'form' => $form->createView()
