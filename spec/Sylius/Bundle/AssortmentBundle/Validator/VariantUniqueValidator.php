9618f4c67f9b1960dd0524ba2c6601ddc0d78896
l3l0 <leszek.prabucki@gmail.com>
2013-02-11 16:35:30 +0100

    Use direct way of define variant dep.

--- spec/Sylius/Bundle/AssortmentBundle/Validator/VariantUniqueValidator.php
+++ spec/Sylius/Bundle/AssortmentBundle/Validator/VariantUniqueValidator.php
@@ -34,6 +34,7 @@ class VariantUniqueValidator extends ObjectBehavior
 
     /**
      * @param Sylius\Bundle\AssortmentBundle\Model\Variant\VariantInterface $conflictualVariant
+     * @param Sylius\Bundle\AssortmentBundle\Model\Variant\VariantInterface $variant
      */
     function it_should_add_violation_if_variant_with_given_property_value_already_exists($variantRepository, $variant, $conflictualVariant, $context)
     {
@@ -52,6 +53,9 @@ class VariantUniqueValidator extends ObjectBehavior
         $this->validate($variant, $constraint);
     }
 
+    /**
+     * @param Sylius\Bundle\AssortmentBundle\Model\Variant\VariantInterface $variant
+     */
     function it_should_not_add_violation_if_variant_with_given_property_value_does_not_exist($variantRepository, $variant, $context)
     {
         $constraint = new VariantUnique(array(
@@ -69,6 +73,7 @@ class VariantUniqueValidator extends ObjectBehavior
 
     /**
      * @param Sylius\Bundle\AssortmentBundle\Model\Variant\VariantInterface $conflictualVariant
+     * @param Sylius\Bundle\AssortmentBundle\Model\Variant\VariantInterface $variant
      */
     function it_should_not_add_violation_if_conflictual_variant_and_validated_one_are_the_same($variantRepository, $variant, $conflictualVariant, $context)
     {
